fb7974d5227675c7cf9b4b76f4c6ae1d553ff2ef
Ramesh Selvam <156361443+rameshdevelop@users.noreply.github.com>
2024-06-04 19:28:16 -0400

    Adding Program.cs file codes

--- Program.cs
+++ Program.cs
@@ -10,6 +10,73 @@ namespace Group6_Assignment_2
     {
         static void Main(string[] args)
         {
+            Product product = new Product(100, "External Hard Drive", 79.99, 70);
+
+            bool continueOperation = true;
+
+            while (continueOperation)
+            {
+                Console.WriteLine("Choose an operation:");
+                Console.WriteLine("1. Increment stock");
+                Console.WriteLine("2. Decrement stock");
+                Console.WriteLine("3. Close");
+                Console.WriteLine("\n");
+
+                string operationChoice = FetchStringFromConsole("Choose Your option:");
+
+                switch (operationChoice)
+                {
+                    case "1":
+                        AddToStock(product);
+                        break;
+                    case "2":
+                        ReduceStock(product);
+                        break;
+                    case "3":
+                        continueOperation = false;
+                        break;
+                    default:
+                        Console.WriteLine("Wrong choice. Please try again.");
+                        break;
+                }
+            }
+        }
+
+        static void AddToStock(Product product)
+        {
+            int increaseAmount = FetchIntegerFromConsole("Enter the quantity to increase the stock by:");
+            product.IncreaseStock(increaseAmount);
+            Console.WriteLine($"Stock increased by {increaseAmount}. New stock: {product.Stock} \n");
+        }
+
+        static void ReduceStock(Product product)
+        {
+            int decreaseAmount = FetchIntegerFromConsole("Enter the quantity to decrease the stock by:");
+            product.DecreaseStock(decreaseAmount);
+            Console.WriteLine($"Stock decreased by {decreaseAmount}. New stock: {product.Stock} \n");
+        }
+
+        static string FetchStringFromConsole(string message)
+        {
+            Console.WriteLine(message);
+            string userInput = Console.ReadLine()?.Trim() ?? "";
+
+            while (String.IsNullOrEmpty(userInput))
+            {
+                userInput = Console.ReadLine()?.Trim() ?? "";
+            }
+            return userInput;
+        }
+
+        static int FetchIntegerFromConsole(string message)
+        {
+            Console.WriteLine(message);
+            int userInput;
+            while (!int.TryParse(Console.ReadLine(), out userInput))
+            {
+                Console.WriteLine("Please Enter a valid input.");
+            }
+            return userInput;
         }
     }
 }
